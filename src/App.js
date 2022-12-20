85ceed4a1f6e97eb7ca13498f87863cc11463186
leeticiafgs <leticia_fernanda93@live.com>
2022-12-20 14:34:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {FiSearch} from 'react-icons/fi'
+import './styles.css';
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep]= useState('');
+
+  async function handleSearch(){
+    if (input===''){
+      alert("Preencha o campo CEP");
+      return;
+    }
+    
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput('');
+    } catch {
+      alert("Ops.. erro ao buscar! Confira se o CEP foi digitado corretamente.");
+      setInput('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input 
+          type="text"
+          placeholder="Digite o CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="FFF"/>
+        </button>
+      </div>
+
+      
+      {Object.keys(cep).length > 0 && (
+            <main className='main'>
+              <h2>CEP: {cep.cep}</h2>
+              <span>Logradouro: {cep.logradouro}</span>
+              <span>Complemento: {cep.complemento}</span>
+              <span>Bairro: {cep.bairro}</span>
+              <span> Cidade: {cep.localidade} - {cep.uf}</span>
+            </main>
+      )}
+
     </div>
   );
 }
